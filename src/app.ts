689facf2149426b994140c9a287355d0178adabe
Shefat Ahmed <shefata69@gmail.com>
2025-02-16 18:10:44 +0600

    routing setup

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
 import express, { Application, Request, Response } from "express"
 import cors from "cors"
+import cookieParser from "cookie-parser"
+import router from "./app/routes"
 const app: Application = express()
 const port = 3000
 
 //parsers
 app.use(express.json())
-app.use(cors())
+app.use(cookieParser());
+
+app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
+
+// application routes
+app.use('/api/v1', router);
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!')
